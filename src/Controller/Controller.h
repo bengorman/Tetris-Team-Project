be977faf7a6e8481c7907db98f8e1bb5abdf1596
Nicholas Tateo <33124193+NickTateo@users.noreply.github.com>
2017-12-03 22:28:24 -0500

    Update Controller.h

--- src/Controller/Controller.h
+++ src/Controller/Controller.h
@@ -1,23 +1,23 @@
-/*
 #ifndef CONTROLLER_H_INCLUDED
 #define CONTROLLER_H_INCLUDED
 
 #include <thread>
+#include <Windows.h>
 
 using namespace std;
-
 
 class Controller
 {
 	private:
-		Thread gameThread;
+		Thread fallThread(GameBoard.descend());
+		Thread keyThread(scanInput());
 
 	public:
-
+		Controller();
+		char scanInput();	
 
 
 };
 
 
 #endif
-*/
